ae48cbec6929dfa427011da3bcccd31cffe42a32
CazinSacha <cazin.lep@gmail.com>
2025-05-02 12:57:22 +0200

    MAJ

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { saveGameState, loadGameState } from './services/dbService';
+import { saveGameState, loadGameState, loadPlayers } from './services/dbService';
 
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
@@ -10,6 +10,8 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [saveMessage, setSaveMessage] = useState('');
+  const [player1, setplayer1] = useState('');
+  const [player2, setplayer2] = useState('');
 
   useEffect(() => {
     const savedGame = loadGameState();
@@ -17,19 +19,26 @@ export default function Board() {
       setSquares(savedGame.squares);
       setXIsNext(savedGame.xIsNext);
     }
+    const savedPlayers = loadPlayers();
+    /*if (savedPlayers.player1) {
+      setplayer1(savedPlayers.player1);
+    }
+    if (savedPlayers.player2) {
+      setplayer2(savedPlayers.player2);
+    }*/
   }, []);
 
   function handleClick(i) {
     if (squares[i]) return;
-    
+
     const newSquares = squares.slice();
     newSquares[i] = xIsNext ? "X" : "O";
-    
+
     setSquares(newSquares);
     setXIsNext(!xIsNext);
-    
+
     saveGameState(newSquares, !xIsNext);
-    
+
     setSaveMessage('Partie sauvegardée !');
     setTimeout(() => setSaveMessage(''), 1000);
   }
@@ -38,17 +47,43 @@ export default function Board() {
     const emptyBoard = Array(9).fill(null);
     setSquares(emptyBoard);
     setXIsNext(true);
-    
+
     saveGameState(emptyBoard, true);
-    
+
     setSaveMessage('Nouvelle partie sauvegardée !');
     setTimeout(() => setSaveMessage(''), 1000);
   }
-  
+
+  function createPlayer(playerNumber) {
+    const namePlayer = prompt(`Nom du joueur ${playerNumber}`);
+    if (namePlayer) {
+      if (playerNumber === 1) {
+        setplayer1(namePlayer);
+        localStorage.setItem('tictactoe_player1', namePlayer);
+      }
+      else {
+        setplayer2(namePlayer);
+        localStorage.setItem('tictactoe_player2', namePlayer);
+      }
+      alert(`Bienvenue ${namePlayer}`);
+    }
+    else {
+      alert("Veuillez entrer un nom valide")
+    }
+  }
   return (
     <div className="game-container">
+
+      <button className='authent-btn' onClick={() => createPlayer(1)}>
+        {player1}
+      </button>
+
+      <button className='authent-btn' onClick={() => createPlayer(2)}>
+        {player2}
+      </button>
+
       <h1>Tic Tac Toe</h1>
-      
+
       <div className="board">
         <div className='board-row'>
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -66,15 +101,15 @@ export default function Board() {
           <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
-      
+
       <div className="game-info">
         Prochain joueur : {xIsNext ? 'X' : 'O'}
       </div>
-      
+
       <button className='reset-btn' onClick={handleReset}>
         Nouvelle partie
       </button>
-      
+
       {saveMessage && <div className="save-message">{saveMessage}</div>}
     </div>
   );
--- src/services/dbService.js
+++ src/services/dbService.js
@@ -1,8 +1,10 @@
-export const saveGameState = (squares, xIsNext) => {
+export const saveGameState = (squares, xIsNext, player1, player2) => {
   try {
     localStorage.setItem('ticTacToe_squares', JSON.stringify(squares));
     localStorage.setItem('ticTacToe_xIsNext', xIsNext);
-    
+    localStorage.setItem('tictactoe_player1', player1);
+    localStorage.setItem('tictactoe_player2', player2);
+
     return true;
   } catch (error) {
     console.error('Erreur de sauvegarde:', error);
@@ -14,17 +16,26 @@ export const loadGameState = () => {
   try {
     const squares = JSON.parse(localStorage.getItem('ticTacToe_squares'));
     const xIsNext = localStorage.getItem('ticTacToe_xIsNext') === 'true';
-    
+
     if (!squares) {
       return null;
     }
-    
+
     return { squares, xIsNext };
   } catch (error) {
     console.error('Erreur de chargement:', error);
     return null;
   }
 };
+
+export const loadPlayers = () => {
+  const player1 = localStorage.getItem('tictacToe_player1');
+  const player2 = localStorage.getItem('tictacToe_player2');
+  console.log('player1', player1);
+  console.log('player2', player2);
+
+  return (player1, player2)
+}
 
 export const initDatabase = async () => {
   return true;
